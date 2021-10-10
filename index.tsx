40e1ef76269e16fe76fefcb110a0caff08faa6e4
yamakado <worldtravel811@gmail.com>
2021-10-10 14:39:22 +0900

    add: react-native-paperのbuttonを追加

--- index.tsx
+++ index.tsx
@@ -1,9 +1,15 @@
 import { registerRootComponent } from "expo";
 import * as React from "react";
 import App from "./src/App";
+import { Provider as PaperProvider } from "react-native-paper";
 
 export default function Main() {
-	return <App />;
+	return (
+		/* ref: https://callstack.github.io/react-native-paper/getting-started.html#usage */
+		<PaperProvider>
+			<App />
+		</PaperProvider>
+	);
 }
 
 registerRootComponent(Main);
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { Button } from "react-native-paper";
 
 export default function App() {
 	return (
 		<View style={styles.container}>
 			<Text>Open up App.tsx to start working on your app!</Text>
 			<StatusBar style="auto" />
+			<Button
+				icon="arrow-right-circle-outline"
+				mode="contained"
+				onPress={() => console.log("Pressed")}
+			>
+				始める
+			</Button>
 		</View>
 	);
 }
