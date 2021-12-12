e89c193dec944efca9fe195852e01ec405821d8b
yamakado <worldtravel811@gmail.com>
2021-12-12 15:17:41 +0900

    add: topから問題文画面への遷移を実装

---
+++ src/constants/common.ts
@@ -0,0 +1,4 @@
+export type RootStackParamList = {
+	App: undefined;
+	Question: undefined;
+};
--- src/screens/App.tsx
+++ src/screens/App.tsx
@@ -2,8 +2,15 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Button } from "react-native-paper";
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../constants/common";
 
 export default function App() {
+	const navigation =
+		useNavigation<
+			NativeStackNavigationProp<RootStackParamList, "Question">
+		>();
 	return (
 		<View style={styles.container}>
 			<Text>Open up App.tsx to start working on your app!</Text>
@@ -12,7 +19,7 @@ export default function App() {
 				/* 利用可能アイコンは、https://callstack.github.io/react-native-paper/icons.html -> See the list of supported icons */
 				icon="arrow-right-circle-outline"
 				mode="contained"
-				onPress={() => console.log("Pressed")}
+				onPress={() => navigation.navigate("Question")}
 				style={{ width: "50%" }}
 				/* https://reactnative.dev/docs/text-style-props */
 				labelStyle={{ fontSize: 30 }}
